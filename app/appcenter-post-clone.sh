36631c85a7ef7c61537df240ec27d2a96166d1ce
Francisco Arguedas <farguedas99@gmail.com>
2022-11-18 16:49:09 -0600

    NET 6.0

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -1,3 +1,4 @@
-#!/usr/bin/env bash
-
-mvn test
+#!/usr/bin/env bash 
+wget https://dot.net/v1/dotnet-install.sh 
+chmod +x dotnet-install.sh 
+./dotnet-install.sh --version 6.0.3 --install-dir "$AGENT_TOOLSDIRECTORY/dotnet"
